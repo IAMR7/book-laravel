962b406cc93ff40743f70ff5ceb04398852d3106
IAMR7 <reza.febri72@gmail.com>
2022-07-15 20:05:28 +0700

    hapus setting

--- resources/views/admin/layout_admin/v_navbar.blade.php
+++ resources/views/admin/layout_admin/v_navbar.blade.php
@@ -27,10 +27,6 @@
             <h6 class="mr-3 mt-2">Halo, {{ Auth::user()->name }} <i class="ti-angle-down"></i> </h6>
           </a>
           <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
-            <a class="dropdown-item">
-              <i class="ti-settings text-primary"></i>
-              Settings
-            </a>
             <form id="logout-form" action="{{ route('logout') }}" method="POST">
               @csrf
               <button type="submit" class="dropdown-item">
